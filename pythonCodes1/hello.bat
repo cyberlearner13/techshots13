2b22a8689089846d8da15a5882b839a74a61e4f3
Arkadeep Mukherjee <arkadeepmukherjee2012@gmail.com>
2017-08-26 21:11:23 +0530

    further explorations

--- pythonCodes1/hello.bat
+++ pythonCodes1/hello.bat
@@ -1,2 +1,2 @@
-@py D:\Projects\Program\Python\AutomateBoringStuffWorkspace\pythonCodes1\hello.py %*
+@C:\Users\USER\AppData\Local\Programs\Python\Python36-32\python.exe D:\Projects\Program\Python\AutomateBoringStuffWorkspace\pythonCodes1\hello.py %*
 @pause
